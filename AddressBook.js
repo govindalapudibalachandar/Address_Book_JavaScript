8313c27bf0dc02eaca91e5ab0d4ab91538e6f455
balachandar <govindalapudibalachandar@gmail.com>
2022-01-31 13:16:54 +0530

    Added:Address_Book_contact file is added

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,25 @@
-console.log("Welcome to AddressBook Contact :");
+
+class Contact
+{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNo;
+    email;
+
+    constructor(firstName , lastName, address, city, state, zip, phoneNo, email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+}
+let contact1 = new Contact("Bala", "Chandar", "rtc colony", "machilipatanamo", "andhraPradesh",500021,9493750386, "balachandar@gmail.com")
+console.log(contact1);
