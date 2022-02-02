f40060c6dab8b45e92851633002e7fe0f1d5b031
balachandar <govindalapudibalachandar@gmail.com>
2022-02-02 14:56:51 +0530

    Added:Count_By_City_State file is added

--- AddressBook.js
+++ AddressBook.js
@@ -142,6 +142,16 @@ function ViewContactByState(){
     AddressBook.filter((contact) => contactsStateMap.set(contact.state, SearchByState(contact.state)));
     return contactsStateMap;
 }
+// UC10 Get Contact Count by city.
+function ContactCountByCity(){
+    AddressBook.filter((contact) => countCityMap.set(contact.city, SearchByCity(contact.city).length));
+    return countCityMap;
+}
+// UC10 Get Contact Count by state.
+function ContactCountByState(){
+    AddressBook.filter((contact) => countStateMap.set(contact.state, SearchByState(contact.state).length));
+    return countStateMap;
+}
 
 function Main() {
 
@@ -187,18 +197,25 @@ function Main() {
   DeleteContact("Bala", "Chandar");
   AddressBook.forEach(contact=>console.log(contact.toString()));
 // UC8 Prints Contact by city.
-console.log(SearchByCity("Mumbai"));
+console.log(SearchByCity("Telangana"));
 // UC8 Prints Contact by state.
-console.log(SearchByState("Telangana"));
+console.log(SearchByState("Mumbai"));
 console.log("---- UC9 ViewsContacts By City----");
 console.log(ViewContactByCity());
 console.log("---- UC9 ViewsContacts By State----");
 console.log(ViewContactByState());
+console.log("----UC10 Contact count by City----");
+console.log(ContactCountByCity());
+console.log("----UC10 Contact count by State----");
+console.log(ContactCountByState());
 }
 // Map to Contacts in a city.
 let contactsCityMap = new Map();
 // Map to Contacts in a state.
 let contactsStateMap = new Map();
-
+// Map to get contacts count by city.
+let countCityMap = new Map();
+// Map to get contacts count by state.
+let countStateMap = new Map();
 Main();
 CountContact();
